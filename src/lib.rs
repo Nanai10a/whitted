c3304f137b875fd2a68c592bc35cd62d5f6cacde
Nanai Jua <me@n7i.dev>
2022-04-09 05:11:35 +0900

    🚧 pre-defined modules.

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+// runtime and network management
+mod adapter;
+
+// constant values
+mod consts;
+
+// defines data structures
+mod model;
+
+// core logics
+mod process;
+
+// request routine
+mod request;
+
+// response routine
+mod response;
+
+// ping - pong verification
+mod verify;
